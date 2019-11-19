b231bec89a0bfff91e24c8521da9e59b770e848c
LinKin-22 <shomnathcse22@gmail.com>
2019-11-19 23:55:38 +0600

    Import all pages

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import './App.css';
 
+import Home from './pages/Home';
+import Rooms from './pages/Rooms';
+import SingleRoom from './pages/SingleRoom';
+import Error from './pages/Error';
+
 function App() {
-  return <>Hello From App</>;
+  return (
+    <>
+    <Home />
+    <Rooms />
+    <SingleRoom />
+    <Error />
+    </>
+  );
 }
 
 export default App;
