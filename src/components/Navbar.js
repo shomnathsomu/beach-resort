3bc26ac20ea1d2f1e140470ec71ad63f0cd10f2c
Shomnath Pramanik <shomnathcse22@gmail.com>
2019-11-20 23:59:01 +0600

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 
 export default class Navbar extends Component {
     render() {
-        return <div>hello from navbar page</div>;
+        return (
+            <div>hello from navbar page</div>
+        );
     }
 }
