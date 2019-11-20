865668ab81a38b3c41c42f03d90cd6844a401a07
LinKin-22 <shomnathcse22@gmail.com>
2019-11-21 00:20:42 +0600

    Change import navbar

--- src/App.js
+++ src/App.js
@@ -5,12 +5,14 @@ import Home from './pages/Home';
 import Rooms from './pages/Rooms';
 import SingleRoom from './pages/SingleRoom';
 import Error from './pages/Error';
+import Navbar from './components/Navbar';
 
 import { Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
     <>
+    <Navbar />
       <Switch>
         <Route path="/" exact component={Home}/>
         <Route path="/rooms/" component={Rooms}/>
