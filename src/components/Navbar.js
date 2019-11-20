329703e106321a5eee19b93c0385e4ce65b800ab
LinKin-22 <shomnathcse22@gmail.com>
2019-11-21 00:22:22 +0600

    Create a customed navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,31 @@
 import React, { Component } from 'react';
+import logo from '../images/logo.svg';
+import { FaAlignRight } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
 
 export default class Navbar extends Component {
+    state = {
+        isOpen: false
+    }
+
+    handleToggle = () => {
+        this.setState({isOpen:!this.state.isOpen})
+    }
+
     render() {
         return (
-            <div>hello from navbar page</div>
+            <nav className="navbar">
+                <div className="nav-center">
+                    <div className="nav-header">
+                        <Link to="/">
+                            <img src={ logo } alt="Beach Resort" />
+                        </Link>
+                        <button type="button" className="nav-btn">
+                            <FaAlignRight className="nav-icon" />
+                        </button>
+                    </div>
+                </div>
+            </nav>
         );
     }
 }
