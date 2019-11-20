320e575bd1e1d8d1ce4b075aa0f747a86a9b5995
LinKin-22 <shomnathcse22@gmail.com>
2019-11-20 23:56:25 +0600

    Use Switch and Route from react dom

--- src/App.js
+++ src/App.js
@@ -6,13 +6,17 @@ import Rooms from './pages/Rooms';
 import SingleRoom from './pages/SingleRoom';
 import Error from './pages/Error';
 
+import { Route, Switch } from 'react-router-dom';
+
 function App() {
   return (
     <>
-    <Home />
-    <Rooms />
-    <SingleRoom />
-    <Error />
+      <Switch>
+        <Route path="/" exact component={Home}/>
+        <Route path="/rooms/" component={Rooms}/>
+        <Route path="/single-room" component={SingleRoom}/>
+        <Route component={Error} />
+      </Switch>
     </>
   );
 }
