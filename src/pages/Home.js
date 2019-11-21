cbc4e38e5ba51d0920b5ac6f0a8e40a901b552bc
LinKin-22 <shomnathcse22@gmail.com>
2019-11-21 22:42:20 +0600

    Create Banner, Link and Service

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,20 @@
 import React from 'react';
+import Hero from '../components/Hero';
+import Banner from '../components/Banner';
+import { Link } from 'react-router-dom';
+import Services from '../components/Services';
 
 export default function Home() {
-    return <div>Hello from home page</div>;
+    return (
+        <>
+            <Hero>
+                <Banner title="luxurious rooms" subtitle="deluxe rooms starting at $299">
+                    <Link to="/rooms" className="btn-primary">
+                        our rooms
+                    </Link>
+                </Banner>
+            </Hero>
+            <Services />
+        </>
+    )
 }
