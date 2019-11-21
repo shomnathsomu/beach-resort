62de0aa31d42a0365777284ca99913cbd1375b0e
LinKin-22 <shomnathcse22@gmail.com>
2019-11-21 22:42:41 +0600

    Create Banner and Link

--- src/pages/Rooms.js
+++ src/pages/Rooms.js
@@ -1,7 +1,16 @@
 import React from 'react';
+import Hero from '../components/Hero';
+import Banner from '../components/Banner';
+import { Link } from 'react-router-dom';
 
 const Rooms = () => {
-    return <div>hello from rooms page</div>;
+    return (
+        <Hero hero="roomsHero">
+            <Banner title="our rooms">
+                <Link to="/" className="btn-primary">Return Home</Link>
+            </Banner>
+        </Hero>
+    )
 }
 
 export default Rooms;
