0ea52620b279a0fc3aa6a718fdefffcc74e096ab
LinKin-22 <shomnathcse22@gmail.com>
2019-11-21 22:39:05 +0600

    Create navbar links

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -20,10 +20,18 @@ export default class Navbar extends Component {
                         <Link to="/">
                             <img src={ logo } alt="Beach Resort" />
                         </Link>
-                        <button type="button" className="nav-btn">
+                        <button type="button" className="nav-btn" onClick={this.handleToggle}>
                             <FaAlignRight className="nav-icon" />
                         </button>
                     </div>
+                    <ul className={ this.state.isOpen ? "nav-links show-nav":"nav-links" }>
+                        <li>
+                            <Link to="/">Home</Link>
+                        </li>
+                        <li>
+                            <Link to="/rooms">Rooms</Link>
+                        </li>
+                    </ul>
                 </div>
             </nav>
         );
