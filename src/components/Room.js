62f6417adc1d6240d2e9dcb8549624a0ec6ef552
LinKin-22 <shomnathcse22@gmail.com>
2019-11-23 23:48:07 +0600

    showed featured rooms

--- src/components/Room.js
+++ src/components/Room.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import { Link } from "react-router-dom";
+import defaultImg from '../images/room-1.jpeg';
 
-export default function Room () {
+export default function Room ({ room }) {
+    
+    const { name, slug, images, price } = room;
+    
     return (
-        <div>
-            hello from room
-        </div>
+        <article className="room">
+            <div className="img-container">
+                <img src={images[0] || defaultImg} alt="single room" />
+                <div className="price-top">
+                    <h6>${price}</h6>
+                    <p>per night</p>
+                </div>
+                <Link to={`/rooms/${slug}`} className="btn-primary room-link" >
+                    Features
+                </Link>
+            </div>
+        </article>
     );
 }
