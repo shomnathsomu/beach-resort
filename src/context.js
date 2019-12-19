1bd8d17c1d3886440ded794e3eda53deed5ed9bc
Shomnath Pramanik <shomnathcse22@gmail.com>
2019-12-19 10:47:06 +0000

    Update state, setState and handleChange event
    filterRooms function as well

--- src/context.js
+++ src/context.js
@@ -9,18 +9,32 @@ export default class RoomProvider extends Component {
         rooms: [],
         sortedRooms: [],
         featuredRooms: [],
-        loading: true
+        loading: true,
+        type: "all",
+        capacity: 1,
+        price: 0,
+        minPrice: 0,
+        maxPrice: 0,
+        minSize: 0,
+        maxSize: 0,
+        breakfast: false,
+        pets: false
     };
 
     componentDidMount() {
         let rooms = this.formatData(items);
         let featuredRooms = rooms.filter(room => room.featured === true);
+        let maxPrice = Math.max(...rooms.map(item => item.price));
+        let maxSize = Math.max(...rooms.map(item => item.size));
 
         this.setState({
             rooms,
             featuredRooms,
             sortedRooms: rooms,
-            loading: false
+            loading: false,
+            price: maxPrice,
+            maxPrice,
+            maxSize
         });
     }
 
@@ -41,10 +55,49 @@ export default class RoomProvider extends Component {
         const room = tempRooms.find(room => room.slug === slug);
         return room;
     }
+
+    handleChange = event => {
+        const target = event.target;
+        const name = event.target.name;
+        const value = event.type === 'checkbox' ? target.checked : target.value;
+        this.setState({
+            [name]: value
+        }, this.filterRooms)
+    }
+
+    filterRooms = () => {
+        let{rooms, type, capacity, price, minSize, maxSize, breakfast, pets} = this.state;
+
+        // all the rooms
+        let tempRooms = [...rooms];
+
+        // transform value
+        capacity = parseInt(capacity);
+
+        // filter by type
+        if(type != 'all') {
+            tempRooms = tempRooms.filter(room => room.type === type)
+        }
+
+        // filter by capacity
+        if(capacity != 1) {
+            tempRooms = tempRooms.filter(room => room.capacity >= capacity)
+        }
+
+        this.setState({
+            sortedRooms: tempRooms
+        })
+    }
     
     render () {
         return (
-            <RoomContext.Provider value={ {...this.state, getRoom: this.getRoom} }>
+            <RoomContext.Provider 
+                value={ {
+                    ...this.state,
+                    getRoom: this.getRoom,
+                    handleChange: this.handleChange
+                } }
+            >
                 {this.props.children}
             </RoomContext.Provider>
         )
@@ -52,5 +105,15 @@ export default class RoomProvider extends Component {
 }
 
 const RoomConsumer = RoomContext.Consumer;
+
+export function withRoomConsumer(Component) {
+    return function ConsumerWrapper(props) {
+        return(
+            <RoomConsumer>
+                {value => <Component {...props} context={value} />}
+            </RoomConsumer>
+        );
+    }
+}
 
 export { RoomProvider, RoomConsumer, RoomContext };
