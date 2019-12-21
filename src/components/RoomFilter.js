512598a4e2c1148b66e8eb429793aa637a31dd37
Shomnath Pramanik <shomnathcse22@gmail.com>
2019-12-21 23:51:36 +0600

    Update filter properties

--- src/components/RoomFilter.js
+++ src/components/RoomFilter.js
@@ -51,6 +51,36 @@ export default function RoomFilter({rooms}) {
                 </div>
                 {/* guests type ends */}
 
+                {/* room price starts */}
+                <div className="form-group">
+                    <label htmlFor="price">room price ${price}</label>
+                    <input type="range" name="price" min={minPrice} max={maxPrice} id="price" value={price} onChange={handleChange} className="form-control"/>
+                </div>
+                {/* room price ends */}
+
+                {/* room size starts */}
+                <div className="form-group">
+                    <label htmlFor="size">room size</label>
+                    <div className="size-inputs">
+                        <input type="number" name="minSize" id="size" value={minSize} onChange={handleChange} className="size-input" />
+                        <input type="number" name="maxSize" id="size" value={maxSize} onChange={handleChange} className="size-input" />
+                    </div>
+                </div>
+                {/* room size ends */}
+
+                {/* extras start */}
+                <div className="form-group">
+                    <div className="single-extra">
+                        <input type="checkbox" name="breakfast" id="breakfast" checked={breakfast} onChange={handleChange} />
+                        <label htmlFor="breakfast">breakfast</label>
+                    </div>
+                    <div className="single-extra">
+                        <input type="checkbox" name="pets" id="pets" checked={pets} onChange={handleChange} />
+                        <label htmlFor="pets">pets</label>
+                    </div>
+                </div>
+                {/* extras end */}
+
             </form>
         </section>
     );
